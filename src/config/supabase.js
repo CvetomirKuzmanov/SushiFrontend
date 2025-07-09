fcc7d349b8f0f25599b1be5e75c4255a942fd79d
CvetomirKuzmanov <cveto.kuzmanov@gmail.com>
2025-07-09 18:21:20 +0300

    added .env

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rfwfqdvlxgmqfxnfcuoy.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmd2ZxZHZseGdtcWZ4bmZjdW95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5MTA5MDEsImV4cCI6MjA2NzQ4NjkwMX0.6HaL6pLFvOs77ZLYIki8ffhTTok1fns1fHO4o_Kcdpk';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
